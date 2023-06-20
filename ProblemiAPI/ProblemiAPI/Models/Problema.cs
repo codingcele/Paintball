48cdcaf7675e9414389181eabaf975ffecd601b3
silvi <silvi@LAPTOP-3O6DIASP>
2023-06-20 17:22:10 +0200

    Aggiungere i file di progetto.

---
+++ ProblemiAPI/ProblemiAPI/Models/Problema.cs
@@ -0,0 +1,12 @@
+﻿namespace ProblemiAPI.Models
+{
+    public class Problema
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        //public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? PossibleSolution { get; set; }
+        public bool IsSolved { get; set; }
+    }
+}
---
+++ ProblemiAPI/ProblemiAPI/Models/ProblemiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProblemiAPI.Models
+{
+    public class ProblemiContext : DbContext
+    {
+        public ProblemiContext(DbContextOptions<ProblemiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Problema> Problemi { get; set; } = null!;
+    }
+}
